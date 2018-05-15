3f0ed2ab848cf0757b4e49955113f56f216fad3d
Cezar Carvalhaes <cezarcarvalhaes@gmail.com>
2018-05-15 18:58:47 -0400

    added logic to server.js to serve app

---
+++ models/goals.js
@@ -0,0 +1,12 @@
+module.exports = function(sequelize, DataTypes) {
+    var Goal = sequelize.define("Goal", {
+        goal: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        activity: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    })
+};
